0f9877d9f45dc021eaa618433237ed9e8ab76445
Hideyasu <kutuzov1228@gmail.com>
2016-11-11 21:05:06 +0900

    sound

--- js/leap.js
+++ js/leap.js
@@ -1,8 +1,52 @@
+function showFlame(flame){
+  console.log(flame);
+}
+function concatData(data) {return "" + data + "<br>";}
+function getHandName(hand) {
+  switch (hand) {
+    case 'right': return "[右手]"; break;
+    case 'left': return "[左手]"; break;
+  }
+}
+function getFingerName(fingerType) {
+  return ["親指　 : ", "人差指: ", "中指　: ", "薬指　: ", "小指　: "][fingerType];
+}
 (function(win, doc) {
-
+    synth.triggerAttackRelease(100, '1m');
+    var synth = new Tone.SimpleSynth().toMaster();
     var controllerOptions = { enableGestures: true };
     Leap.loop(controllerOptions, function(frame) {
+        frameString = "";
 
+        for (var i = 0, len = frame.hands.length; i < len; i++) {
+          // 「手」単位の処理
+          hand = frame.hands[i];
+          frameString += concatData(getHandName(hand.type));
+
+            // 「指」単位の処理
+            finger = hand.fingers[3];
+            fingerString =""
+            fingerString += finger.tipPosition[1]; // 指先の位置
+            frameString += concatData(fingerString)
+            showFlame(fingerString)
+            if(fingerString>300){
+              synth.triggerAttackRelease('C4', '1m');
+            }else if(fingerString>250){
+              synth.triggerAttackRelease('B3', '1m');
+            }else if(fingerString>200){
+              synth.triggerAttackRelease('A3', '1m');
+            }else if(fingerString>150){
+              synth.triggerAttackRelease('G3', '1m');
+            }else if(fingerString>100){
+              synth.triggerAttackRelease('F3', '1m');
+            }else if(fingerString>50){
+              synth.triggerAttackRelease('E3', '1m');
+            }else if(fingerString>1){
+              synth.triggerAttackRelease('D3', '1m');
+            }
+            //「C5」の音を「2分音符」で発音
+            synth.triggerAttackRelease(100, '1m');
+        }
         // Display Gesture object data
         if (frame.gestures.length > 0) {
             for (var i = 0; i < frame.gestures.length; i++) {
