fc6bc702b37300bbed71b61b31210296f89cc09c
Hideyasu <kutuzov1228@gmail.com>
2016-11-12 12:34:45 +0900

    onkai

--- js/leap.js
+++ js/leap.js
@@ -1,15 +1,8 @@
 function showFlame(flame){
   console.log(flame);
 }
-function concatData(data) {return "" + data + "<br>";}
-function getHandName(hand) {
-  switch (hand) {
-    case 'right': return "[右手]"; break;
-    case 'left': return "[左手]"; break;
-  }
-}
-function getFingerName(fingerType) {
-  return ["親指　 : ", "人差指: ", "中指　: ", "薬指　: ", "小指　: "][fingerType];
+function setPlay(time, note) {
+  synth.triggerAttackRelease(note, '8n', time);
 }
 (function(win, doc) {
     //synth.triggerAttackRelease(100, '1m');
@@ -21,31 +14,75 @@ function getFingerName(fingerType) {
         for (var i = 0, len = frame.hands.length; i < len; i++) {
           // 「手」単位の処理
           hand = frame.hands[i];
-          frameString += concatData(getHandName(hand.type));
 
             // 「指」単位の処理hide
             finger = hand.fingers[3];
-            fingerString =""
-            fingerString += finger.tipPosition[1]; // 指先の位置
-            frameString += concatData(fingerString)
-            showFlame(fingerString)
-            if(fingerString>300){
-              synth.triggerAttackRelease('C4', '1m');
-            }else if(fingerString>250){
-              synth.triggerAttackRelease('B3', '1m');
-            }else if(fingerString>200){
-              synth.triggerAttackRelease('A3', '1m');
-            }else if(fingerString>150){
-              synth.triggerAttackRelease('G3', '1m');
-            }else if(fingerString>100){
-              synth.triggerAttackRelease('F3', '1m');
-            }else if(fingerString>50){
-              synth.triggerAttackRelease('E3', '1m');
-            }else if(fingerString>1){
-              synth.triggerAttackRelease('D3', '1m');
+            fingerX = finger.tipPosition[0];
+            fingerY = finger.tipPosition[1]; // 指先の位置
+            fingerZ = finger.tipPosition[2];
+            showFlame(fingerZ)
+
+            var ySound="";
+            if(fingerY>271){
+              ySound = "C4"
+            }else if(fingerY>233){
+              ySound = "B3"
+            }else if(fingerY>195){
+              ySound = "A3"
+            }else if(fingerY>157){
+              ySound = "G3"
+            }else if(fingerY>119){
+              ySound = "F3"
+            }else if(fingerY>81){
+              ySound = "E3"
+            }else if(fingerY>43){
+              ySound = "D3"
+            }else if(fingerY>6){
+              ySound = "C3"
             }
-            //「C5」の音を「2分音符」で発音
-            //synth.triggerAttackRelease(100, '1m');
+
+            var xSound="";
+            if(fingerX>140){
+              xSound = "C4"
+            }else if(fingerX>105){
+              xSound = "B3"
+            }else if(fingerX>70){
+              xSound = "A3"
+            }else if(fingerX>35){
+              xSound = "G3"
+            }else if(fingerX>0){
+              xSound = "F3"
+            }else if(fingerX>-35){
+              xSound = "E3"
+            }else if(fingerX>-70){
+              xSound = "D3"
+            }else{
+              xSound = "C3"
+            }
+
+            var zSound="";
+            if(fingerZ>100){
+              zSound = "C4"
+            }else if(fingerZ>50){
+              zSound = "B3"
+            }else if(fingerZ>0){
+              zSound = "A3"
+            }else if(fingerZ>-50){
+              zSound = "G3"
+            }else if(fingerZ>-100){
+              zSound = "F3"
+            }else if(fingerZ>-150){
+              zSound = "E3"
+            }else if(fingerZ>-200){
+              zSound = "D3"
+            }else{
+              zSound = "C3"
+            }
+            
+            //synth.triggerAttackRelease(xSound, '4n');
+            //synth.triggerAttackRelease(ySound, '4n');
+            synth.triggerAttackRelease(ySound, '4n');
+            break;
         }
         // Display Gesture object data
         if (frame.gestures.length > 0) {
