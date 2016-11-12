689744682e62a2e44130fd90864a28f68e281352
fuchi15 <sakuya815@gmail.com>
2016-11-12 12:18:10 +0900

    音階の場所の色が変わる

--- js/leap.js
+++ js/leap.js
@@ -1,15 +1,22 @@
-function showFlame(flame){
-  console.log(flame);
+function showFlame(flame) {
+    console.log(flame);
 }
-function concatData(data) {return "" + data + "<br>";}
+
+function concatData(data) { return "" + data + "<br>"; }
+
 function getHandName(hand) {
-  switch (hand) {
-    case 'right': return "[右手]"; break;
-    case 'left': return "[左手]"; break;
-  }
+    switch (hand) {
+        case 'right':
+            return "[右手]";
+            break;
+        case 'left':
+            return "[左手]";
+            break;
+    }
 }
+
 function getFingerName(fingerType) {
-  return ["親指　 : ", "人差指: ", "中指　: ", "薬指　: ", "小指　: "][fingerType];
+    return ["親指　 : ", "人差指: ", "中指　: ", "薬指　: ", "小指　: "][fingerType];
 }
 (function(win, doc) {
     //synth.triggerAttackRelease(100, '1m');
@@ -19,30 +26,40 @@ function getFingerName(fingerType) {
         frameString = "";
 
         for (var i = 0, len = frame.hands.length; i < len; i++) {
-          // 「手」単位の処理
-          hand = frame.hands[i];
-          frameString += concatData(getHandName(hand.type));
+            // 「手」単位の処理
+            hand = frame.hands[i];
+            frameString += concatData(getHandName(hand.type));
 
             // 「指」単位の処理hide
             finger = hand.fingers[3];
-            fingerString =""
+            fingerString = ""
             fingerString += finger.tipPosition[1]; // 指先の位置
-            frameString += concatData(fingerString)
+            frameString += concatData(fingerString);
+            if ($('li').hasClass('is_show')) {
+                $('.is_show').removeClass('is_show');
+            }
             showFlame(fingerString)
-            if(fingerString>300){
-              synth.triggerAttackRelease('C4', '1m');
-            }else if(fingerString>250){
-              synth.triggerAttackRelease('B3', '1m');
-            }else if(fingerString>200){
-              synth.triggerAttackRelease('A3', '1m');
-            }else if(fingerString>150){
-              synth.triggerAttackRelease('G3', '1m');
-            }else if(fingerString>100){
-              synth.triggerAttackRelease('F3', '1m');
-            }else if(fingerString>50){
-              synth.triggerAttackRelease('E3', '1m');
-            }else if(fingerString>1){
-              synth.triggerAttackRelease('D3', '1m');
+            if (fingerString > 300) {
+                synth.triggerAttackRelease('C4', '1m');
+                $('.bottom-do').addClass('is_show');
+            } else if (fingerString > 250) {
+                synth.triggerAttackRelease('B3', '1m');
+                $('.si').addClass('is_show');
+            } else if (fingerString > 200) {
+                synth.triggerAttackRelease('A3', '1m');
+                $('.ra').addClass('is_show');
+            } else if (fingerString > 150) {
+                synth.triggerAttackRelease('G3', '1m');
+                $('.so').addClass('is_show');
+            } else if (fingerString > 100) {
+                synth.triggerAttackRelease('F3', '1m');
+                $('.fa').addClass('is_show');
+            } else if (fingerString > 50) {
+                synth.triggerAttackRelease('E3', '1m');
+                $('.mi').addClass('is_show');
+            } else if (fingerString > 1) {
+                synth.triggerAttackRelease('D3', '1m');
+                $('.re').addClass('is_show');
             }
             //「C5」の音を「2分音符」で発音
             //synth.triggerAttackRelease(100, '1m');
@@ -68,7 +85,13 @@ function getFingerName(fingerType) {
                             swipeDirection = "down";
                         }
                     }
-                    console.log(swipeDirection)
+                    console.log(swipeDirection);
+                    if (swipeDirection === "right") {
+
+                    }
+                    if (swipeDirection === "left") {
+
+                    }
                 }
             }
         }
